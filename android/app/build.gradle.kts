7144072e3f293923d00f30c1f8f6364809e6be03
Rawane GAYE <rawanex@yahoo.fr>
2026-08-12 18:12:50 +0200

    maj version android 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 android {
     namespace = "com.xoolibeut.livreur"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -29,7 +29,7 @@ android {
     defaultConfig {
         applicationId = "com.xoolibeut.livreur"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
